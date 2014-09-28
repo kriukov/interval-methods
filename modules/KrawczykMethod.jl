911acea2688daf9b314e5f24e7547ad2e1912dd6
Nikolay Kryukov <kriukov@gmail.com>
2014-09-27 22:27:31 -0500

    Krawczyk method module with tests

---
+++ modules/KrawczykMethod.jl
@@ -0,0 +1,56 @@
+## Krawczyk method, based on Tucker p. 86
+
+module KrawczykMethod
+export krawczyk, differentiate, Interval, rad, diam, mid, mig, mag, belong, hd, hull, isect, isectext
+
+using AutoDiff
+
+println("Syntax: krawczyk(function, Interval(lo, hi), precision)")
+
+# Outside wrapping function was made in order for it to clean up the array of roots every time
+function krawczyk(f::Function, a::Interval, bigprec::Integer)
+
+
+arr_a = Interval[]
+
+function krawczyk_internal(f::Function, a::Interval, bigprec::Integer)
+
+	set_bigfloat_precision(bigprec)
+	tol = 100eps(BigFloat)
+	
+	K(x) = mid(x) - (Interval(1)//differentiate(f, mid(x)))*f(mid(x)) + (1 - (Interval(1)//differentiate(f, mid(x)))*differentiate(f, x))*(x - mid(x))
+	
+	# If a is symmetric, i.e., mid(a) = 0, the process may stall. The initial interval should be slightly asymmetrized then
+	if mid(a) == 0
+		a = Interval(a.lo, a.hi + 0.0001*mag(a))
+	end
+	
+	if isect(a, K(a)) != false
+		@show diam(a)
+		@show diam(K(a))
+
+
+		if diam(K(a)) < tol
+			if diam(K(a)) < diam(a)
+				println("Unique zero in $(K(a))")
+				push!(arr_a, K(a))
+			else
+				println("Maybe a zero in $(K(a))")
+			end
+		
+		else
+			krawczyk_internal(f, Interval(a.lo, mid(a)), bigprec)
+			krawczyk_internal(f, Interval(mid(a), a.hi), bigprec)			
+		end
+
+	end
+	
+	return arr_a
+end
+
+
+return krawczyk_internal(f, a, bigprec)
+end
+
+#end of module
+end
---
+++ modules/tests-krawczyk.jl
@@ -0,0 +1,46 @@
+using Base.Test
+using KrawczykMethod
+
+# Transcendental true roots were found using Wolfram Mathematica
+
+a = Interval(-20, 20)
+
+enclosure(x) = Interval(x - 5000*eps(BigFloat), x + 5000*eps(BigFloat))
+
+function checkroots(f, trueroots)
+	roots = krawczyk(f, a, 64)
+	# Check if both ends of the found interval lie within the 10eps-enclosure of the true root
+	n = 0
+	for i = 1:length(roots)
+		if belong(roots[i].lo, enclosure(trueroots[i])) == false && belong(roots[i].hi, enclosure(trueroots[i])) == false
+			n += 1
+		end
+	end
+	n == 0
+end	
+
+f(x) = x^2 - 4
+trueroots = [-2, 2]
+@test checkroots(f, trueroots)
+
+f(x) = 5040 - 3828x - 2356x^2 + 1009x^3 + 200x^4 - 62x^5 - 4x^6 + x^7
+trueroots = [-6, -4, -2, 1, 3, 5, 7]
+@test checkroots(f, trueroots)
+
+f(x) = exp(x^2) - 4
+a = Interval(-4, 4)
+trueroots = [-sqrt(log(4)), sqrt(log(4))]
+@test checkroots(f, trueroots)
+
+# The next function is undefined in [-1, 1], so we split the interval manually
+f(x) = log(x^2 - 1) + x + 1
+a = Interval(-10, -1.01)
+trueroots = [-3.274277505524962, -1.7894889253208424]
+@test checkroots(f, trueroots)
+
+f(x) = log(x^2 - 1) + x + 1
+a = Interval(1.01, 10)
+trueroots = [1.0617135926109396]
+@test checkroots(f, trueroots)
+
+
