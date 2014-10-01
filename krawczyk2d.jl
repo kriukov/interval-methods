6f735b2d96388945c96de071d4c2c8a79a6e157d
David P. Sanders <dpsanders@gmail.com>
2014-10-01 13:50:17 -0500

    Trying to modify Kraw 2D

--- krawczyk2d.jl
+++ krawczyk2d.jl
@@ -5,9 +5,10 @@ function krawczyk2d(f, a::Array{Interval, 1}, bigprec::Integer)
 arr_a = Array{Interval, 1}[]
 
 set_bigfloat_precision(bigprec)
-tol = (1e10)*eps(BigFloat)	
+#tol = (1e-10)*eps(BigFloat)
+tol = 1e-10
 
-I = [Interval(1) Interval(0); Interval(0) Interval(1)]	
+I = [Interval(1) Interval(0); Interval(0) Interval(1)]
 Y(x) = make_intervals(inv(mid(jacobian(f, mid(x)))))
 M(x) = I - Y(x)*jacobian(f, x)
 K(x) = make_intervals(mid(x)) - Y(x)*make_intervals(f(mid(x))) + M(x)*(x - make_intervals(mid(x)))
@@ -15,40 +16,41 @@ K(x) = make_intervals(mid(x)) - Y(x)*make_intervals(f(mid(x))) + M(x)*(x - make_
 function krawczyk2d_internal(f, a::Array{Interval, 1}, bigprec::Integer)
 
 
-	
+
 	# If a is symmetric, i.e., mid(a) = 0, the process may stall. The initial interval should be slightly asymmetrized then
 	#if mid(a) == 0
 	#	a = Interval(a.lo, a.hi + 0.0001*mag(a))
 	#end
-	
-	if isect(a, K(a)) != false
-		@show a
-		@show diam(a)
+
+  Ka = isect(a, K(a))
+	if Ka != false
+		#@show a
+		#@show diam(a)
 
 		d = diam(a)
-		dK = diam(K(a))
-		
-		if d[1] < tol && d[2] < tol #d == dK 
+		dK = diam(Ka)
+
+		if d[1] < tol && d[2] < tol #d == dK
 			if dK[1] < d[1] && dK[2] < d[2] # Put <= instead of <
-				println("Unique zero in $(K(a))")
-				push!(arr_a, (K(a)))
+				println("Unique zero in $Ka")
+				push!(arr_a, Ka)
 			else
-				println("Maybe a zero in $(K(a))")
+				println("Maybe a zero in $Ka")
 			end
-		
+
 		else
 			M1 = Y(a)
-			if mid(det2(M(K(a)))) <= mid(det2(M(a)))
-				krawczyk2d_internal(f, K(a), bigprec)
-				
-			else 
+			if mid(det2(M(Ka))) <= mid(det2(M(a)))
+				krawczyk2d_internal(f, Ka, bigprec)
+
+			else
 				krawczyk2d_internal(f, make_intervals(mid(a)) - M1*make_intervals(f(mid(a))) + (I - M1*jacobian(f, a))*(a - make_intervals(mid(a))), bigprec)
 			end
 
 		end
 
 	end
-	
+
 	return arr_a
 end
 
