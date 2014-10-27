08cc5c46d5626e1c109d75061143de55bf2db2d0
Nikolay Kryukov <kriukov@gmail.com>
2014-10-27 15:16:56 -0600

    Krawczyk2D tests

--- modules/tests-krawczyk2d.jl
+++ modules/tests-krawczyk2d.jl
@@ -5,23 +5,37 @@ using KrawczykMethod2D
 
 # Transcendental true roots were found using Wolfram Mathematica
 
-a = [Interval(-10, 10), Interval(-10, 10)]
-
-enclosure(x) = Interval(x - 5000*eps(BigFloat), x + 5000*eps(BigFloat))
+enclosure(x) = Interval(x - 1e-9, x + 1e-9)
 
 function checkroots(f, trueroots)
 	roots = krawczyk2d(f, a)
-	# Check if both ends of the found interval lie within the 10eps-enclosure of the true root
+	# Check if both ends of each found interval lie within the enclosure of the true root
 	n = 0
 	for i = 1:length(roots)
-		if belong(roots[i][1].lo, enclosure(trueroots[i])) == false && belong(roots[i][1].hi, enclosure(trueroots[i])) == false
-			n += 1
+		for j = 1:2 # For 2D
+			if belong(roots[i][j].lo, enclosure(trueroots[i][j])) == false && belong(roots[i][j].hi, enclosure(trueroots[i][j])) == false
+				n += 1
+			end
 		end
 	end
 	n == 0
 end	
 
 f(x) = [x[1]^2 + x[2]^2 - 1, x[1] - x[2]^2]
-a = [Interval(0.5, 0.8), Interval(0.6, 0.9)]
-krawczyk2d(f, a, 64)
-trueroots = [(sqrt(5) - 1)/2, sqrt((sqrt(5) - 1)/2)]
+a = [Interval(0, 1), Interval(0, 1)]
+trueroots = Array[[(sqrt(5) - 1)/2, sqrt((sqrt(5) - 1)/2)]]
+@test checkroots(f, trueroots)
+
+
+f(x) = [20 - 20x[1] - x[2], x[1] - x[2]/20 - 1e-9*exp(x[2]/0.052)]
+a = [Interval(0, 2), Interval(0, 2)]
+trueroots = Array[[0.9464142468335176, 1.0717150633296477]]
+@test checkroots(f, trueroots)
+
+
+f(x) = [0.5*(-(17.76x[1] - 103.79x[1]^2 + 229.62x[1]^3 - 226.31x[1]^4 + 83.72x[1]^5) + x[2]), 0.2*(-x[1] - 1.5x[2] + 1.2)]
+a = [Interval(0, 1), Interval(0, 1)]
+trueroots = Array[[0.06263595920972119, 0.758242693860186], [0.2853687241300338, 0.6097541839133109], [0.8844295888702942, 0.21038027408647064]]
+@test checkroots(f, trueroots)
+
+
