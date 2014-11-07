9cda0af067442369595ecee1df3c1bd01929e381
Nikolay Kryukov <kriukov@gmail.com>
2014-11-06 19:47:49 -0600

    Atahualpa's algorithm updated; need to resolve problem with eff()

--- atahualpa.jl
+++ atahualpa.jl
@@ -1,47 +1,14 @@
+# https://github.com/johnmyleswhite/ContinuedFractions.jl
+using ContinuedFractions
+
+conv(x) = convergents(ContinuedFraction(x), Rational)
 
 function frac(alpha, epsilon)
-	
-	irr0 = alpha
-
-	arr_a = Integer[]
-
-	n = 0
-	while n <= 38 # change!
-		a = int(floor(alpha))
-		print("$a, ")
-		push!(arr_a, a)
-		alpha = 1/(alpha - a)
+	n = 1
+	while abs(alpha*den(conv(alpha)[n]) - num(conv(alpha)[n])) < epsilon
 		n += 1
 	end
-
-
-	h = Integer[]
-	push!(h, 0, 1)
-	for i = 3:40 # change!
-		push!(h, arr_a[i-2]*h[i-1] + h[i-2])
-	end
-	shift!(h)
-	shift!(h)
-
-
-	k = Integer[]
-	push!(k, 1, 0)
-	for i = 3:40 # change!
-		push!(k, arr_a[i-2]*k[i-1] + k[i-2])
-	end
-	shift!(k)
-	shift!(k)
-
-
-	function first_solution(alpha, epsilon)
-		n = 1
-		while abs(alpha*k[n] - h[n]) < epsilon
-			n +=1	
-		end
-		(k[n], h[n])
-	end
-
-	first_solution(irr0, epsilon)
+	(den(conv(alpha)[n]), num(conv(alpha)[n]))
 end
 
 function eff(m, b, epsilon)
@@ -53,14 +20,40 @@ function eff(m, b, epsilon)
 		else
 			(q, p) = frac(m, 2*(1 - bb))
 		end
-		bb = m*q + b - int(m*q + b)
+		bb = m*q + b - ifloor(m*q + b)
 		kn = kn + q		
 	end
 	q = kn
-	p = int(q*m) + 1
+	p = ifloor(m*q) + 1
 	return (q, p)	
 end
 
-
-
+function first_collision(x, y, vx, vy, delta)
+	m = vy/vx
+	b = y - m*x
+	if vx > 0 && vy > 0
+		(q, p) = eff(m, b, delta)
+		p = ifloor(m*q) + 1
+	elseif vx < 0 && vy > 0
+		m = -m
+		(q, p) = eff(m, b, delta)
+		p = ifloor(m*q) + 1
+		m = -m
+		q = -q
+	elseif vx < 0 && vy < 0
+		b = 1 - b
+		(q, p) = eff(m, b, delta)
+		b = 1 - b
+		p = -(ifloor(m*q) + 1)
+		q = -q
+	elseif vx > 0 && vy < 0
+		b = 1 - b
+		m = -m
+		(q, p) = eff(m, b, delta)
+		b = 1 - b
+		m = -m
+		p = -(ifloor(m*q) + 1)
+	end
+	return (q, p)
+end
 
