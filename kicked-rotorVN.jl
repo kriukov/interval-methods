43f00062597cb89fb41176541f51f0750413b296
Nikolay Kryukov <kriukov@gmail.com>
2016-08-06 01:36:50 -0500

    Attempt to do inverse maps 1231: divergence too big

---
+++ kicked-rotorVN.jl
@@ -0,0 +1,96 @@
+## Kicked rotor: periodic orbits calculation
+# x is a vector (x[1], x[2]) = (x, p)
+
+using KrawczykMethod2DVN
+
+# Base function for the kicked rotor, x[1] is x, x[2] is p
+f0(x, k) = mod2pi([x[1] + k*sin(x[1]) + x[2], k*sin(x[1]) + x[2]])
+f(x, k) = [x[1] + k*sin(x[1]) + x[2], k*sin(x[1]) + x[2]]
+
+# Function composition while keeping k arbitrary
+compose(f::Function, g::Function) = (x, k) -> f(g(x, k), k)
+
+# T^n (x) function - T(T(T(...T(x)))) n times
+function T(x, k, n)
+	F = f
+	for i = 1:n-1
+		F = compose(F, f)
+	end
+	return F(x, k)
+end
+
+
+# T^n (x) function - T(T(T(...T(x)))) n times
+function T0(x, k, n)
+	F = f0
+	for i = 1:n-1
+		F = compose(F, f0)
+	end
+	return F(x, k)
+end
+
+
+# n-periodic points equation
+h(x, k, n) = T(x, k, n) - x
+
+# Equation to find ONLY n-periodic points
+
+function g(x, k, n)
+	g1 = 1
+	for i = 1:n-1
+		g1 = g1*h(x, k, i)
+	end
+	h(x, k, n)/g1
+end
+
+
+# Functions to output zeros of n-th order given the K and the limits (x1, x2) and (p1, p2)
+#zeros(K, n, x1, x2, p1, p2, precision) = krawczyk2d_mod(x -> h(x, K, n), [Interval(x1, x2), Interval(p1, p2)], precision)
+
+top(x, k, n) = ceil(hi(T(x, k, n))/2pi)*2pi
+
+function roots(k, n, x1, x2, p1, p2, precision)
+	array_sol = IntervalBox[]
+	a = IntervalBox(Interval(x1, x2), Interval(p1, p2))
+	for p = 0:top(a, k, n)[1]/2pi
+		for q = 0:top(a, k, n)[2]/2pi
+			array_sol = vcat(array_sol, krawczyk2d(x -> h(x, k, n) - IntervalBox(Interval(2pi*p), Interval(2pi*q)), a, precision))
+		end
+	end
+	array_sol_nodupes = unique(array_sol)
+	array_sol_nodupes
+end
+
+# Print out 2-periodic points for k = 1
+#println(mid(zeros(1, 3, 0, 2pi, 0, 2pi, 64)))
+
+
+# Function to output the plot points
+
+#= turned off
+for n = 1:40
+	x0 = [2pi*rand(), 2pi*rand()]
+	k = 1
+	for i = 1:10000
+		output = T0(x0, k, i)
+		println("$(output[1]) $(output[2])")
+	end
+end
+=#
+
+
+# Print out periodic points for
+
+#=
+println("1-periodic: ")
+println(mid(roots(1, 1, 0, 2pi, 0, 2pi, 64)))
+
+println("2-periodic: ")
+println(mid(roots(1, 2, 0, 2pi, 0, 2pi, 64)))
+
+println("3-periodic: ")
+println(mid(roots(1, 3, 0, 2pi, 0, 2pi, 64)))
+
+println("4-periodic: ")
+println(mid(roots(1, 4, 0, 2pi, 0, 2pi, 64)))
+=#
